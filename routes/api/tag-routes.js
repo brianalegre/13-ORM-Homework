3edc6e2a89c6c80e3d4c2d199a6d2aec4d2b1e6e
Brian Alegre <brialegre@yahoo.com>
2022-06-22 16:04:48 -0700

    Added findAll and findByPK routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,15 +2,36 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// find all tags
+// be sure to include its associated Product data
+// NOTES: Added async
+router.get('/', async (req, res) => {
+  try {
+    const allTags = await Tag.findAll({
+      include: [{ model: Product }],
+    })
+    res.status(200).json(allTags)
+  } catch (err) {
+    res.status(500).json('Something went wrong', err)
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+
+// find a single tag by its `id`
+// be sure to include its associated Product data
+// NOTES: Added async
+router.get('/:id', async (req, res) => {
+  try {
+    const singleTag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    })
+    if (!singleTag) {
+      res.status(404).json({ message: 'No tag found with that id!' })
+    }
+    res.status(200).json(singleTag)
+  } catch (err) {
+    res.status(500).json('Something went wrong', err)
+  }
 });
 
 router.post('/', (req, res) => {
