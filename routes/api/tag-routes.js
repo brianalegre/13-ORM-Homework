1efe56a0b1ea99c068c409df5e088253e2d57856
Brian Alegre <brialegre@yahoo.com>
2022-06-22 20:45:05 -0700

    Added PUT route for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,7 +52,20 @@ router.post('/', async (req, res) => {
 
 // update a tag's name by its `id` value
 // NOTES: Added async
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const updateTag = Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!updateTag) {
+      res.status(404).json({ message: 'No Tag found with this id!' })
+    }
+    res.status(200).json('Tag has been updated')
+  } catch (err) {
+    res.status(500).json('Something went wrong', err)
+  }
 });
 
 // delete on tag by its `id` value
