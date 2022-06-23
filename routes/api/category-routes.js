319052f0d918c746d52a2faf4c8373670aa399b2
Brian Alegre <brialegre@yahoo.com>
2022-06-22 21:27:40 -0700

    Changed status code in category POST route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,8 +37,21 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+
+// create a new category
+/* req.body
+{
+  "category_name": "newTagFor15"
+}
+*/
+// NOTES: Added async
+router.post('/', async (req, res) => {
+  try {
+    const newCatgetory = await Category.create(req.body);
+    res.status(200).json(newCatgetory)
+  } catch (err) {
+    res.status(400).json('Something went wrong', err)
+  }
 });
 
 // update a category by its `id` value
